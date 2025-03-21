26a968d6d929a415e6016f68bc02810a69d1bf5b
wrestlemantuck <wrestlemantuck@gmail.com>
2025-03-21 17:34:18 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.wrestle.gorillatag.WrestlesMenu";
+        public const string Name = "Angelessrose menu";
+        public const string Description = "This uses IIDK's template.";
+        public const string Version = "1.5.8";
+    }
+}
