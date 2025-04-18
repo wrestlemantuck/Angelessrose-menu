535bd3f4c5372c2b8b6274211f835c505d5f34fc
wrestlemantuck <wrestlemantuck@gmail.com>
2025-04-18 00:45:40 -0500

    Update StartupMods.cs

--- Menu/StartupMods.cs
+++ Menu/StartupMods.cs
@@ -1,4 +1,4 @@
-﻿using BepInEx;
+using BepInEx;
 using HarmonyLib;
 using StupidTemplate.Classes;
 using StupidTemplate.Notifications;
@@ -17,13 +17,12 @@ namespace StupidTemplate.Menu
 {
     internal class StartupMods : MonoBehaviour
     {
-        async void Start()
+        void Start()
         {
-            await Task.Delay(2000);
             CustomMOTD();
             SettingsMods.CheckVer();
         }
-        public static void CustomMOTD()
+        void CustomMOTD()
         {
             GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/motd (1)").GetComponent<TextMeshPro>().text = "Thank you for using Angelessrose menu";
             GameObject.Find("Environment Objects/LocalObjects_Prefab/TreeRoom/motdtext").GetComponent<TextMeshPro>().text = "All stuff made by wrestle";
